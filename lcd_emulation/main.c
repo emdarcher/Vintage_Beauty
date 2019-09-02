a06d756af91a121b1bf3ff3a9686d69c4ac4bf84
Ethan Durrant <5387347+emdarcher@users.noreply.github.com>
2019-09-02 14:59:48 -0600

    working on the LCD command/request processing

--- lcd_emulation/main.c
+++ lcd_emulation/main.c
@@ -17,6 +17,7 @@
 //calculates the timer clock setup
 #define TIMER_CLOCKS    ((((CPU_FREQ)/(PRESCALER))*(PERIOD))/(TICKS_PER_SEC))
 #define TIMER_RES       65536 //16 bits
+#define TIMER_BITS      (TIMER_RES - TIMER_CLOCKS)
 
 //how many bits in one column
 #define COL_BITS            7
@@ -55,19 +56,29 @@
 #define DATA_CLK_PIN        P1_7
 
 //LCD control commands, the base bit mask for each command.
-#define CMD_CLEAR_DISPLAY   0x01
-#define CMD_RETURN_HOME     0x02
-#define CMD_ENTRY_MODE_SET  0x04
-#define CMD_DISP_ON_OFF     0x08
-#define CMD_DISP_SHIFT      0x10
-#define CMD_FUNC_SET        0x20
-#define CMD_SET_CGRAM_ADDR  0x40
-#define CMD_SET_DDRAM_ADDR  0x80
+#define CMD_CLEAR_DISPLAY       0x01
+#define CMD_CLEAR_DISPLAY_MASK      (CMD_CLEAR_DISPLAY - 1)
+#define CMD_RETURN_HOME         0x02
+#define CMD_RETURN_HOME_MASK        (CMD_RETURN_HOME - 1)
+#define CMD_ENTRY_MODE_SET      0x04
+#define CMD_ENTRY_MODE_SET_MASK     (CMD_ENTRY_MODE_SET - 1)
+#define CMD_DISP_ON_OFF         0x08
+#define CMD_DISP_ON_OFF_MASK        (CMD_DISP_ON_OFF - 1)
+#define CMD_DISP_SHIFT          0x10
+#define CMD_DISP_SHIFT_MASK         (CMD_DISP_SHIFT - 1)
+#define CMD_FUNC_SET            0x20
+#define CMD_FUNC_SET_MASK           (CMD_FUNC_SET - 1)
+#define CMD_SET_CGRAM_ADDR      0x40
+#define CMD_SET_CGRAM_ADDR_MASK     (CMD_SET_CGRAM_ADDR - 1)
+#define CMD_SET_DDRAM_ADDR      0x80
+#define CMD_SET_DDRAM_ADDR_MASK     (CMD_SET_DDRAM_ADDR - 1)
 
 //masks for read/write etc. bottom two bits are values of RS and RW
+#define OTHER_CMD           0x00
 #define READ_BUSY_FLAG      0x01
 #define WRITE_DATA          0x02
 #define READ_DATA           0x03
+#define NO_REQUEST          0xFF
 
 //bit numbers for flags in various commands
 #define ENTRY_MODE_I_D_BIT      1
@@ -76,15 +87,20 @@
 #define DISPLAY_CURSOR_BIT      1
 #define DISPLAY_BLINK_BIT       0
 #define DISPLAY_SHIFT_BIT       3
-#define DISPLAY_SHITT_DIR_BIT   2
+#define DISPLAY_SHIFT_DIR_BIT   2
 #define FUNC_SET_DATA_LEN_BIT   4
 #define FUNC_SET_N_LINES_BIT    3
 #define FUNC_SET_FONT_BIT       2
 
 
 //the selection and control pins
-#define RS_PIN      P1_1
-#define RW_PIN      P1_0
+#define RS_PIN          P1_1
+#define RW_PIN          P1_0
+//the port of the RW and RS pins
+#define RW_RS_PORT      P1
+//bit mask with both the RS and RW pins
+#define RW_RS_BIT_MASK  ((1<<1)|(1<<0))
+
 //the enable pin, attached to external interrupt 1 (INT0) on P3.2
 //make sure to invert the E signal before input using an inverter
 //as the interrupt is triggered on LOW, but LCD spec is triggered on HIGH
@@ -93,7 +109,12 @@
 #define DB_PORT     P0
 //the busy state output pin (this is done on DB7)
 #define BUSY_PIN    P0_7
+//busy pin bit mask
+#define BUSY_PIN_BIT    7
+#define BUSY_PIN_BIT_MASK   (1<<BUSY_PIN_BIT)
 
+//mask for setting cursor pixels
+#define CURSOR_MASK     0x01
 
 //bits for outputting the columns on the I/O port
 const uint8_t col_bits[CHAR_COLS] = {
@@ -114,6 +135,14 @@ volatile uint8_t ins_reg;
 //variable to store the DR (data register)
 volatile uint8_t data_reg;
 
+
+//specifies increment or decrement mode
+volatile __bit inc_dec_bit = 1;
+
+volatile uint8_t cgram_addr;
+
+volatile uint8_t ddram_addr;
+
 //variable to store the AC (address counter)
 volatile uint8_t addr_counter;
 
@@ -126,6 +155,9 @@ volatile __bit blink_state = 0;
 //triggers the blinking tick function
 volatile __bit blink_flag = 0;
 
+//sets if the cursor is active
+volatile __bit cursor_state = 0;
+
 //sets if blinking block is currently on
 __bit block_on = 0;
 
@@ -137,6 +169,24 @@ volatile __bit enable_flag = 0;
 //sets the enabled state
 __bit enable_state = 0;
 
+//shift direction 1 == right, 0 == left
+//volatile __bit shift_dir = 1;
+
+//sets data length 1 == 8 bits, 0 == 4 bits
+volatile __bit data_len = 1;
+
+//the number of lines of the display
+//1 == 2 lines, 0 == 1 line
+volatile __bit display_lines = 0;
+
+//display on flag
+volatile __bit display_on = 1;
+
+//flag for if there is a new request to process
+volatile __bit new_request_flag = 0;
+
+//store current request
+volatile uint8_t request_type = 0;
 
 /*
  * Memory Map example for one byte of the 8051's bit memory
@@ -188,7 +238,13 @@ void shift_out_column(uint8_t col_num);
 uint8_t get_char_col(uint8_t ch, uint8_t col);
 void load_col_buff(uint8_t col);
 void load_out_str_buff(uint8_t * in_str, bool is_str);
-//inerrupt setup functions
+
+void write_ddram_addr(uint8_t addr, uint8_t data);
+uint8_t read_ddram_addr(uint8_t addr);
+
+void process_command(uint8_t cmd);
+
+//interrupt setup functions
 void init_timer0_interrupt(void);
 void disable_timer0_interrupt(void);
 void init_int0_interrupt(void);
@@ -196,6 +252,128 @@ void init_int0_interrupt(void);
 //tick functions
 void blinking_tick(void);
 void column_update_tick(void);
+void process_request_tick(void);
+
+uint8_t read_ddram_addr(uint8_t addr){
+    //read from the given address
+    if(addr < CHAR_COUNT){
+        return out_str_buff[addr]; 
+    } else {
+        return 0;
+    }
+}
+
+void write_ddram_addr(uint8_t addr, uint8_t data){
+    //write data into the buffer
+    if(addr < CHAR_COUNT){
+        //copy the data 
+        out_str_buff[addr] = data;
+    }
+}
+
+void process_request_tick(void){
+   
+    if(new_request_flag != 0){
+
+        switch(request_type){
+            case OTHER_CMD:
+                //call function to process the command
+
+                break;
+            
+            case WRITE_DATA:
+                //write to the buff
+                //copy the data from the register and into the buffer
+                write_ddram_addr(addr_counter, data_reg);
+                //increment the address counter
+                addr_counter++;
+                //unset busy flag
+                busy_flag = 0;
+                break;
+
+            case READ_DATA:
+                //read from the buff to the data port
+                DB_PORT = read_ddram_addr(addr_counter);
+                //decrement address counter
+                addr_counter--;
+                //unset the busy flag
+                busy_flag = 0;
+                break;
+        }
+        //unset busy flag by here 
+        //unset the new request flag as well
+        new_request_flag = 0;
+    } 
+}
+
+void process_command(uint8_t cmd){
+   
+    //check the masks for each command, going from the MSB and down 
+    //then execute the matching command
+    
+    if(cmd & ~CMD_SET_DDRAM_ADDR_MASK){
+        //get the address bits and put them in the address counter
+        addr_counter = (cmd & ~CMD_SET_DDRAM_ADDR);
+    
+    } else if(cmd & ~CMD_SET_CGRAM_ADDR_MASK){
+        //not implementing CGRAM right now
+        //so nothing
+    
+    } else if(cmd & ~CMD_FUNC_SET_MASK){
+        //set data length
+        data_len = (cmd & (1<<FUNC_SET_DATA_LEN_BIT));
+        //set line num
+        display_lines = (cmd & (1<<FUNC_SET_N_LINES_BIT));
+        //set font
+        //ignore this as our display only supports one font, 5x7
+    
+    } else if(cmd & ~CMD_DISP_SHIFT_MASK){
+        //set the shift direction
+        bool shift_dir = (cmd & (1<<DISPLAY_SHIFT_DIR_BIT));
+        //display or cursor
+        if(!(cmd & (1<<DISPLAY_SHIFT_BIT))){
+            //shift the cursor 
+            if(shift_dir){
+                //shift right (increment addr_counter)
+                addr_counter++;
+            } else {
+                //shift left (decrement addr_counter) 
+                addr_counter--;
+            }
+        } else {
+            //shift the display 
+            //TODO
+        }
+
+    
+    } else if(cmd & ~CMD_DISP_ON_OFF_MASK){
+        //set flag
+        display_on = (cmd & (1<<DISPLAY_ON_BIT));
+        //set cursor on or off
+        cursor_state = (cmd & (1<<DISPLAY_CURSOR_BIT));
+        //set if blinking
+        blink_state = (cmd & (1<<DISPLAY_BLINK_BIT));
+
+    } else if(cmd & ~CMD_ENTRY_MODE_SET_MASK){
+        //set inc or dec 
+        inc_dec_bit = (cmd & (1<<ENTRY_MODE_I_D_BIT));
+        //shift 
+
+    } else if(cmd & ~CMD_RETURN_HOME_MASK){
+        //return address counter to address zero
+        addr_counter = 0x00;
+
+    } else if(cmd & ~CMD_CLEAR_DISPLAY_MASK){
+        //loop through the buffer
+        for(uint8_t i = 0; i<CHAR_COUNT; i++){
+            //fill with space characters to clear the display
+            out_str_buff[i] = ' ';
+        }
+        //return address counter to address zero
+        addr_counter = 0x00;
+    }
+
+}
 
 void init_int0_interrupt(void){
     //set the pin value to 1 
@@ -210,8 +388,15 @@ void init_int0_interrupt(void){
 
 
 void column_update_tick(void){
-    //variable to increment the current column
+    //static variable to increment the current column
     static uint8_t col_inc;
+
+    if(display_on == 0){
+        //turn off all columns
+        COL_PORT &= COLS_OFF_MASK;
+        //break out of the update since we don't need to update the display
+        return;
+    }
     
     if(disp_col_update_flag != 0){
         //wait for the flag
@@ -271,6 +456,10 @@ void load_col_buff(uint8_t col){
         } else {
             //load the character columns from the ROM
             out_buff[i] = get_char_col(out_str_buff[i], col);
+            //set the cursor line bit if cursor is active
+            if((cursor_state == 1) && (i == addr_counter)){
+                out_buff[i] |= CURSOR_MASK;
+            }
         }
    
         //break when we reach zero 
@@ -354,9 +543,9 @@ void init_timer0_interrupt(void){
     //set the timer bits and enable the interrupt
     
     //high bits
-    TH0 = (TIMER_RES - TIMER_CLOCKS) >> 8;
+    TH0 = TIMER_BITS >> 8;
     //low bits
-    TL0 = (TIMER_RES - TIMER_CLOCKS) & 0x00ff;
+    TL0 = TIMER_BITS & 0x00ff;
     TMOD = 0x01; //set mode
     EA = 1;
     ET0 = 1;
@@ -386,9 +575,9 @@ void main(void){
 void Timer0_ISR(void) __interrupt 1 {
     //set the clock bits again 
     //high bits
-    TH0 = (TIMER_RES - TIMER_CLOCKS) >> 8;
+    TH0 = TIMER_BITS >> 8;
     //low bits
-    TL0 = (TIMER_RES - TIMER_CLOCKS) & 0x00ff;
+    TL0 = TIMER_BITS & 0x00ff;
 
     //set our flag
     disp_col_update_flag = 1;
@@ -416,4 +605,49 @@ void INT0_ISR(void) __interrupt 0 {
     //set the busy flag
     busy_flag = 1;
 
+    //set enable flag
+    //enable_flag = 1;
+
+    //read RW and RS
+    uint8_t rw_rs_vals = RW_RS_PORT & RW_RS_BIT_MASK;
+
+    //a switch to determine command type
+    switch(rw_rs_vals){
+        case OTHER_CMD:
+            //set the new request flag
+            new_request_flag = 1;
+            //read in the instruction data from the port
+            ins_reg = DB_PORT;
+            //set request
+            request_type = OTHER_CMD;
+            break;
+        
+        case READ_BUSY_FLAG:
+            //output the busy flag state and addr counter value on data port
+            DB_PORT = (busy_flag << BUSY_PIN_BIT) | addr_counter;            
+            break;
+
+        case WRITE_DATA:
+            //copy data into the DR
+            data_reg = DB_PORT;
+            //set the new request flag
+            new_request_flag = 1;
+            //set the request for write
+            request_type = WRITE_DATA;
+            break;
+
+        case READ_DATA:
+            //set the new request flag
+            new_request_flag = 1;
+            //set request for read
+            request_type = READ_DATA;
+            break;
+
+        default:
+            //unset the new request flag
+            new_request_flag = 0;
+            //default to NO_REQUEST
+            request_type = NO_REQUEST;
+            break;
+    }
 }
